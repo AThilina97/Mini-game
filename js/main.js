69a9666b61d1e08f7bb06ba0d2869332bc98d00f
AThilina97 <thilinarangaprasad@gmail.com>
2023-05-22 00:53:05 +0530

    initiate and implemented js code to move dino left ,right and jump

---
+++ js/main.js
@@ -0,0 +1,108 @@
+const part =document.createElement('div');
+part.classList.add('element');
+document.body.append(part);
+
+let jump=false;
+let moveLeft=false;
+let moveRight=false;
+document.body.addEventListener('keydown',(eventData)=>{
+    if(eventData.code=='Space')jump=true;
+    if(eventData.code=='ArrowLeft')moveLeft=true;
+    if(eventData.code=='ArrowRight')moveRight=true;
+        
+});
+document.body.addEventListener('keyup',(eventData)=>{
+    if(eventData.code=='ArrowLeft') moveLeft=false;
+    if(eventData.code=='ArrowRight') moveRight=false;
+        
+});
+let dx=0;
+function jumping(){
+    part.style.top=700-700*(Math.sin(Math.PI/180*dx))+"px";
+    dx++;
+    if(dx==180) {
+        dx=0;
+        jump=false;
+    }
+};
+let right=0;
+function movingRight(){
+    part.style.left=right+"px";
+    if(right==document.body.offsetWidth-150){
+        right=document.body.offsetWidth-150;
+    }else{
+        right=right+2;
+    }
+};
+
+function movingleft(){
+    part.style.left=right+"px";
+    if(right<=0){
+        right=0;
+    }else{
+        right=right-2;
+    }
+};
+
+const tmr=setInterval(() => {
+    if(jump){
+        jumping();
+    }
+    if(moveRight){
+        movingRight();
+    }
+    if(moveLeft){
+        movingleft();
+    }
+    
+}, 1);
+
+let i=1
+setInterval(() => {
+    if(!(jump || moveLeft || moveRight)){
+        part.style.backgroundImage=`url('img/png/Idle (${i}).png')`;
+        if(i==10) i=0;
+        i++;
+    }
+    
+}, 100);
+let j=0;
+let state=0;
+setInterval(() => {
+    if(moveRight){
+        if(state===1){
+            part.style.transform='scaleX(1)';
+            state=0;
+        }
+        part.style.backgroundImage=`url('img/png/Walk (${j}).png')`;
+        if(j>=10) j=0;
+        j++;
+
+    }
+    
+}, 50);
+let l=0
+setInterval(() => {
+    if(moveLeft){
+        part.style.backgroundImage=`url('img/png/left/Walk (${l}).png')`;
+        if(state===0){
+            part.style.transform='scaleX(-1)';
+            state=1;
+
+        }
+        if(l>=10) l=0;
+        l++;
+
+    }
+    
+}, 50);
+let k=0;
+setInterval(() => {
+    if(jump){
+        part.style.backgroundImage=`url('img/png/Jump (${k}).png')`;
+        if(k>=12) k=0;
+        k++;
+
+    }  
+}, 50);
+
